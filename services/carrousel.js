e5977b89b7fe570e85df56d13007518d80128ced
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-07-09 21:06:43 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -10,6 +10,9 @@ const PI220_CAROUSEL = {
 
   images: [
 
+
+    "https://i.ibb.co/wrLhCwbK/Prof-Dr-Jesse-Jessen.jpg",
+    "https://i.ibb.co/nqYZPHYp/Master-Course-Prof-Dr-Jesse-Jessen.png",
     "https://i.ibb.co/HT3QNVb4/Prof-Dr-Jesse-Jessen.png",
     "https://i.ibb.co/8LfmbcCj/Prof-Dr-Jesse-Jessen.jpg",
     "https://i.ibb.co/d0DLpjD3/Prof-Dr-Jesse-Jessen-Master-Program.png",
