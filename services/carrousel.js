dde61ddc2c1b23c382d5bb884cc49c737fca5e14
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-08-05 14:37:46 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -10,6 +10,8 @@ const PI220_CAROUSEL = {
 
   images: [
 
+
+     "https://i.ibb.co/mFdS3C5L/PI220-MISSION-CONTINUES.png",
      "https://i.ibb.co/QvDSnXg0/PI220-FAMILY-GROWS.png",
      "https://i.ibb.co/sp2v6CsL/PI220-MISSION-CONTINUES.png",
      "https://i.ibb.co/WWbbyY4W/PI220-MISSION-CONTINUES.jpg",
