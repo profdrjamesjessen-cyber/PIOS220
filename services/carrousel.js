c006b662414d220ed7a0a79299d7a31077a0ccc6
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-09-21 09:23:20 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -11,7 +11,7 @@ const PI220_CAROUSEL = {
   images: [
 
      "https://raw.githubusercontent.com/profdrjamesjessen-cyber/PIOS220/6684528f651aed826d2ef3f93a7c6ee078048bb5/blog/LETTER.png",
-    
+     "https://raw.githubusercontent.com/profdrjamesjessen-cyber/PIOS220/blob/cdc3c181eaec41f27d4454c74bd35384846be622/blog/Captura%20de%20pantalla%202026-09-21%20092140.png",
      
      
   ],
