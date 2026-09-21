04bb27f22d98581c931e04fa04fae8ad9f7eb918
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-09-21 14:24:18 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -10,7 +10,7 @@ const PI220_CAROUSEL = {
 
   images: [
 
-
+    "https://i.ibb.co/jPqz1G3P/PI220-OPERATION-SUCCESS.png",
     "https://i.ibb.co/SFx1nxM/EMAIL-ME-ONLY-IF-SCORE-ABOVE-80-100-NATIVE-SPANISH-LADIES-18-25-ONLY.png",
     "https://i.ibb.co/608hjrJR/THE-PI220.png",
     "https://raw.githubusercontent.com/profdrjamesjessen-cyber/PIOS220/784086461db0bbc99abc2878e67daf09c6ae5599/blog/LETTER.png",
