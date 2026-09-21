b096281b63b3053cc1c41a4f8535388d86cb238e
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-09-21 10:12:49 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -10,9 +10,11 @@ const PI220_CAROUSEL = {
 
   images: [
 
-    "https://github.com/profdrjamesjessen-cyber/PIOS220/blob/784086461db0bbc99abc2878e67daf09c6ae5599/blog/LETTER.png",
-    "https://github.com/profdrjamesjessen-cyber/PIOS220/blob/784086461db0bbc99abc2878e67daf09c6ae5599/blog/21092026.png",
-    "https://github.com/profdrjamesjessen-cyber/PIOS220/blob/784086461db0bbc99abc2878e67daf09c6ae5599/blog/CHAPTER12.png",
+     
+    "https://raw.githubusercontent.com/profdrjamesjessen-cyber/PIOS220/blob/784086461db0bbc99abc2878e67daf09c6ae5599/blog/LETTER.png",
+    "https://raw.githubusercontent.com/profdrjamesjessen-cyber/PIOS220/blob/784086461db0bbc99abc2878e67daf09c6ae5599/blog/21092026.png",
+    "https://raw.githubusercontent.com/profdrjamesjessen-cyber/PIOS220/blob/784086461db0bbc99abc2878e67daf09c6ae5599/blog/CHAPTER12.png",
+
      
   ],
 
