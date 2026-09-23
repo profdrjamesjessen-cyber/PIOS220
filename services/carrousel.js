beea5bfd645cebffd26924991eab0d8773a6f0ef
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-09-23 16:03:40 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -10,7 +10,7 @@ const PI220_CAROUSEL = {
 
   images: [
 
-
+    "https://i.ibb.co/d0kP7QFM/PROF-DSC-JESSE-JESSEN-GUITAR-YAMAHA-SLG-200-S-SPONSORED-BY-PI220.png",
     "https://i.ibb.co/k6cTHkSN/LETTER.png",
     "https://i.ibb.co/NP6c2g5/THE-PI220-TOP-SECRET-FILES.png",
     "https://i.ibb.co/NRJqfy5/THE-PI220-TOP-SECRET-FILES.png",
