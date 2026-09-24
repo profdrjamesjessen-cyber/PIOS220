b5be1ae4ab07e004683d2ca5670144a66c913d54
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-09-24 10:29:13 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -10,6 +10,8 @@ const PI220_CAROUSEL = {
 
   images: [
 
+
+    "https://i.ibb.co/VpVS0WHR/PI220.png",
     "https://i.ibb.co/sJD6ffXT/PROF-DSC-JESSE-JESSEN.png",
     "https://i.ibb.co/RThsrZXG/PROF-DSC-JESSE-JESSEN-POKERSTARS-BLACK-FRIDAY.png",
     "https://i.ibb.co/rK275mYM/50-000-BN.png",
