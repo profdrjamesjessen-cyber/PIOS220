796c37899984aee2c254ace21da7c40913b5c8c0
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-09-25 16:55:17 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -12,7 +12,7 @@ const PI220_CAROUSEL = {
 
 
      
-    "https://i.ibb.co/Y4JXDyps/PROF-DSC-JESSE-JESSEN.png", 
+    "https://i.ibb.co/Y4JXDyps/PROF-DSC-JESSE-JESSEN-I-KILLED-SPIDERMAN-NY-IT-WAS-JUST-AN-UGLY-SPIDER.png", 
     "https://i.ibb.co/CphkVRgN/PARALLEL-INDUSTRIES-220.png",
     "https://i.ibb.co/xqyDgK1r/THE-PI220-MEMORANDUM.png",
     "https://i.ibb.co/6JG0C2fD/WORLD-WAR-III-LEGAL-EXECUTIONS-DEATH-PENALTY.png",
