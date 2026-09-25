d5c86369c27393d7dcfda0b5abb05a95c0efbb00
PROF DR JESSE JESSEN <profdrjamesjessen@gmail.com>
2026-09-25 15:16:00 +0200

    Update carrousel.js

--- services/carrousel.js
+++ services/carrousel.js
@@ -13,11 +13,13 @@ const PI220_CAROUSEL = {
     
     "https://i.ibb.co/CphkVRgN/PARALLEL-INDUSTRIES-220.png",
     "https://i.ibb.co/xqyDgK1r/THE-PI220-MEMORANDUM.png",
+    "https://i.ibb.co/5g7mRGvH/SPECIAL-OPERATIONS.png",
     "https://i.ibb.co/GD7bBmR/WORLD-WAR-III-LEGAL-EXECUTIONS.png",
     "https://i.ibb.co/VcBCVhTh/THE-PI220-PORTFOLIO-WARCRAFT-1-4-INDEPENDENT-NO-CROSS-CONTAMINATION.png",
     "https://i.ibb.co/fzV6NmqN/THE-PI220-PORTFOLIO-WARCRAFT-2-4-NO-CROSS-CONTAMINATION.png",
     "https://i.ibb.co/HT9tcNSk/THE-PI220-TOP-SECRET-FILES.png",
     "https://i.ibb.co/fdwkzTy7/THE-PI220-TOP-SECRET-FILES.png",
+    "https://i.ibb.co/zTR9ksbc/THE-PI220-TOP-SECRET-FILES.png",
     "https://i.ibb.co/27dky4q6/PROF-DSC-JESSE-JESSEN.png",
     "https://i.ibb.co/sJD6ffXT/PROF-DSC-JESSE-JESSEN.png",
     "https://i.ibb.co/RThsrZXG/PROF-DSC-JESSE-JESSEN-POKERSTARS-BLACK-FRIDAY.png",
